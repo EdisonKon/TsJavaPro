d0b8dbce34b63bc838a2e756ac9c71b81a400fb1
EdisonKon <huainian_cl@126.com>
2019-01-24 16:29:03 +0800

    test

--- src/main/java/TsThreads/TestManyProc.java
+++ src/main/java/TsThreads/TestManyProc.java
@@ -12,15 +12,24 @@ import java.util.concurrent.*;
 public class TestManyProc {
     public static void main(String[] args) {
         MyCallable callable1 = new MyCallable(1000);                       // 要执行的任务
-        MyCallable callable2 = new MyCallable(2000);
+        MyCallable callable2 = new MyCallable(200000);
 
         FutureTask<String> futureTask1 = new FutureTask<String>(callable1);// 将Callable写的任务封装到一个由执行者调度的FutureTask对象
         FutureTask<String> futureTask2 = new FutureTask<String>(callable2);
 
         ExecutorService executor = Executors.newFixedThreadPool(2);        // 创建线程池并返回ExecutorService实例
+        Future fx = executor.submit(futureTask2);
+
         executor.execute(futureTask1);  // 执行任务
         executor.execute(futureTask2);
 
+        try {
+            fx.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         while (true) {
             try {
                 if(futureTask1.isDone() && futureTask2.isDone()){//  两个任务都完成
---
+++ src/main/java/zOther/zOther.java
@@ -0,0 +1,11 @@
+package zOther;
+
+/**
+ * @description: 描述
+ * @author: dekai.kong (dekai.kong@luckincoffee.com)
+ * @date: 2019-01-24 16:28
+ */
+
+
+public class zOther {
+}
