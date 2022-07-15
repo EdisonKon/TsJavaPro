10b1442053d1878ee189b1e0d99f4d516ea5040a
EdisonKon <huainian_cl@126.com>
2022-07-15 14:52:04 +0800

    ts 状态模式

---
+++ src/main/java/TsDesignModel/statemodel/LiftState.java
@@ -0,0 +1,41 @@
+package TsDesignModel.statemodel;
+
+import org.junit.Test;
+
+/**
+ * @author dekai.kong
+ * @difficult
+ * @create 2022-07-15 14:38
+ * @from
+ **/
+public abstract class LiftState {
+    //定义一个环境角色，也就是封装状态的变化引起的功能变化
+    protected Context context;
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    //电梯开门动作
+    public abstract void open();
+
+    //电梯关门动作
+    public abstract void close();
+
+    //电梯运行动作
+    public abstract void run();
+
+    //电梯停止动作
+    public abstract void stop();
+
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.setLiftState(new ClosingState());
+
+        context.open();
+        context.close();
+        context.run();
+        context.stop();
+    }
+}
+
